288b400a9a65a29bf4a4392d54d0c3b2dc75959b
Arpit Deshmukh <arpitdeshmukh21@gmail.com>
2025-08-27 12:26:56 +0530

    authentication and basics of backend done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,43 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express';
-
-import {createServer} from "node:http";
-
-import {Server} from "socket.io";
+import { createServer } from "node:http";
 import mongoose from "mongoose";
-import {connectToSocket} from './controllers/socketManager.js';
-
+import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
-
-
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.set("port",process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb" , extended:true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users",userRoutes);
-// app.use("/api/v2/users",newUserRoutes) #tfor next versions
+app.use("/api/v1/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully!");
+});
 
 
-const start = async() =>{
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://arpit_deshmukh:Nokia%4045@visuals-db.p2nvbbw.mongodb.net/")
-    console.log(`MONGO connected DB host : ${connectionDb.connection.host}`)
 
-    server.listen(app.get("port"),()=>{
-        console.log("Listening to port 8080")
-    })
-    // app.listen(8080,()=>{
-    //     console.log("Listening on port 8080")
-    // })
-}
+const start = async () => {
+  const connectionDb = await mongoose.connect(MONGO_URI);
+  console.log(`MONGO connected DB host : ${connectionDb.connection.host}`);
+
+  server.listen(app.get("port"), () => {
+    console.log("Listening to port 8080");
+  });
+};
 
 start();
