cabb593008d0b2fa3dc0a4a08cce8c27f3caf7d8
Arpit Deshmukh <arpitdeshmukh21@gmail.com>
2025-08-30 01:27:54 +0530

    backend-rendering done

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,12 +2,12 @@ import axios, { HttpStatusCode } from "axios"
 import { useNavigate } from "react-router-dom";
 
 import { createContext, useContext,useState } from "react";
-
+import server from "../environment";
 
 export const AuthContext  = createContext({});
 
 const client = axios.create({
-    baseURL:"http://localhost:8080/api/v1/users"
+    baseURL:`${server}/api/v1/users`
 })
 
 export const AuthProvider = ({children}) =>{
--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://visuals_backend.onrender.com" :
+    
+   " https://visuals-backend-v2.onrender.com"
+    :
 
     "http://localhost:8000"
 
